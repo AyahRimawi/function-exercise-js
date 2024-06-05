2f6318dac7af2b403b3d963983581c1c8e2697c7
ayahrimawi <ayaalrimawi406@gmail.com>
2024-06-05 23:34:22 +0300

    function-exercise-js

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # function-exercise-js
+# JavaScript Task: Various methods were utilized to accomplish the task.
+- Loop
+- if
+- function
+- alert
+- console.log
+- prompt
+- join
+- split
---
+++ app.js
@@ -0,0 +1,59 @@
+
+// let num = prompt('please enter number')
+// function reverseNumber(num) {
+//     alert(num.split('').reverse().join(''))
+// }
+// reverseNumber(num)
+
+
+    
+// for (let i = 0; i<=15; i++) {
+//    if (i%2==0) {
+//      console.log('even '+i);
+//    }
+//    else{
+//     console.log('odd '+i);
+//    }
+//     }
+
+// let word = prompt('please enter number')
+// function alphabetical(word) {
+//     alert(word.toLowerCase().split('').sort().join(''))
+// }
+// alphabetical(word)
+
+
+
+
+// let num = prompt('please enter number')
+// function Number(num) {
+//     let newNum ="";
+//     for (let i = 0; i < num.length; i++) {
+//         if (num[i] % 2 == 0 && num[i+1] %2 ==0 ) {
+//             newNum += num[i] + "-";
+//         }
+//         else{
+//             newNum += num[i];
+//         }  
+//     }
+//     return newNum
+// }
+// alert(Number(num))
+
+
+
+
+
+let age = prompt('please enter number')
+function Agechecker(age) {
+   let x = (age>=18) ? alert("Adult")  : alert("Minor");
+    return x
+}
+Agechecker(age)
+
+
+
+
+
+
+
